7d807ee2b22ea27cb54a4bf7d524a8754b6094a9
Terell-Davis <terellbdavis@gmail.com>
2024-01-30 11:33:16 -0500

    Add Themes & Theme switch button, suavemente.

--- theme-switch.js
+++ theme-switch.js
@@ -1,4 +1,4 @@
-document.getElementById('themeToggle').addEventListener('click', function() {
+document.getElementById('theme-switch').addEventListener('click', function() {
     var body = document.body;
     if (body.classList.contains('dark-theme')) {
         body.classList.remove('dark-theme');
