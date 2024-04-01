784acdac22d18e2362e8398758a358e94caa5f03
Terell-Davis <54454563+Terell-Davis@users.noreply.github.com>
2024-04-01 15:56:45 -0400

    got bored, added more themes

--- my-portfolio/src/components/Themeswitch.js
+++ my-portfolio/src/components/Themeswitch.js
@@ -15,9 +15,14 @@ const ThemeSwitch = () => {
         <div className={styles.dropdownContent}>
           <button onClick={() => changeTheme('default-theme')}>Default</button>
           <button onClick={() => changeTheme('light-theme')}>Light</button>
-		  <button onClick={() => changeTheme('purple-theme')}>Purple</button>
-		  <button onClick={() => changeTheme('armour-theme')}>Armour</button>
-		  <button onClick={() => changeTheme('simpatico-theme')}>Simpatico</button>
+		      <button onClick={() => changeTheme('purple-theme')}>Purple</button>
+		      <button onClick={() => changeTheme('armour-theme')}>Armour</button>
+		      <button onClick={() => changeTheme('simpatico-theme')}>Simpatico</button>
+          <button onClick={() => changeTheme('sonic-theme')}>Sonic</button>
+          <button onClick={() => changeTheme('tails-theme')}>Tails</button>
+          <button onClick={() => changeTheme('knuckles-theme')}>Kunckles</button>
+          <button onClick={() => changeTheme('shadow-theme')}>Shadow</button>
+          <button onClick={() => changeTheme('eggman-theme')}>Eggman</button>
         </div>
       )}
     </div>
