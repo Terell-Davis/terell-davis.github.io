6b2f325b37daf9a326e73ecbb9f29f0165eeb73c
Terell-Davis <54454563+Terell-Davis@users.noreply.github.com>
2024-04-04 12:19:41 -0400

    Fixed NavBar elements to float left

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,12 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
-      <Link to="/" className={styles.link}>Home</Link>
-	    <Link to="/projects" className={styles.link}>Projects</Link>
-      <Link to="/inspirobot" className={styles.link}>InspiroBot</Link>
-      <Link to="/contact" className={styles.link}>Contact</Link>
+      <div className={styles.linksContainer}>
+        <Link to="/" className={styles.link}>Home</Link>
+	      <Link to="/projects" className={styles.link}>Projects</Link>
+        <Link to="/inspirobot" className={styles.link}>InspiroBot</Link>
+        <Link to="/contact" className={styles.link}>Contact</Link>
+      </div>
 	    <ThemeSwitch />
     </nav>
   );
