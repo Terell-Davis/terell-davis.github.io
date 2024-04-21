3a0cc22a8747f3af59787a1ebd59d0ab86e620e2
Terell-Davis <54454563+Terell-Davis@users.noreply.github.com>
2024-04-21 16:01:34 -0400

    credit to icons8 to well icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.content}>
         © {new Date().getFullYear()} Terell Bernard Davis. All rights reserved.
+        <p>Icons provided by <a href="https://icon8.com" target="_blank" rel="noopener noreferrer">Icons8</a>.</p>
       </div>
     </footer>
   );
